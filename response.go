195d865879ab59227f1b30484a9705f52775a8a4
Elixa-Mae Pesa <eapesa@gmail.com>
2018-04-11 10:29:23 +0800

    Added response formatting

--- response.go
+++ response.go
@@ -1,19 +1,86 @@
 package main
 
 import (
-  "encoding/json"
+  "encoding/xml"
 )
 
-type Response struct {
-  Code string `json:"code,omitempty"`
-  Message string `json:"message,omitempty"`
-  Params string `json:"params,omitempty"`
+type VxmlPrompt struct {
+  XMLName   xml.Name    `xml:"vxml"`
+  Form      Form        `xml:"form"`
 }
 
-var (
-  OK, _            = json.Marshal(&Response{Code:"OK",            Message:"Operation completed successfully"})
-  NOT_FOUND, _     = json.Marshal(&Response{Code:"NOT_FOUND",     Message:"Does not exist"})
-  INVALID_INPUT, _ = json.Marshal(&Response{Code:"INVALID_INPUT", Message:"Missing input"})
-  SERVER_ERROR, _  = json.Marshal(&Response{Code:"SERVER_ERROR",  Message:"Server encountered error"})
-  UNAUTHORIZED, _  = json.Marshal(&Response{Code:"UNAUTHORIZED",  Message:"Unauthorized"})
-)
+type Form struct {
+  Id        string    `xml:"id,attr"`
+  Name      string    `xml:"name,attr"`
+  Field     Field     `xml:"field"`
+  Filled    Filled    `xml:"filled"`
+  Catch     Catch     `xml:"catch"`
+  Property  Property  `xml:"property"`
+}
+
+type Field struct {
+  FieldName  string  `xml:"name,attr"`
+  Prompt     string  `xml:"prompt>field"`
+}
+
+type Filled struct {
+  Assign    Assign  `xml:"assign"`
+  Goto      Goto    `xml:"goto"`
+}
+
+type Assign struct {
+  AssignName  string  `xml:"name,attr"`
+  Expr        string  `xml:"expr,attr"`
+}
+
+type Goto struct {
+  Next    string  `xml:"next,attr"`
+}
+
+type Catch struct {
+  Event   string  `xml:"event,attr"`
+  Prompt  string  `xml:"prompt>catch"`
+  Goto    Goto    `xml:"goto"`
+}
+
+type Property struct {
+  PropertyName  string  `xml:"name,attr"`
+  PropertyValue string  `xml:"value,attr"`
+}
+
+func prompt_reply() []byte {
+  vxml := &VxmlPrompt{
+    Form: Form{
+      Id: "Output",
+      Name: "Output",
+      Field: Field{
+        FieldName: "oc_Output",
+        Prompt: "Your transaction is being processed.",
+      },
+      Filled: Filled{
+        Assign: Assign{
+          AssignName: "",
+          Expr: "",
+        },
+        Goto: Goto{
+          Next: "",
+        },
+      },
+      Catch: Catch{
+        Event: "nomatch",
+        Prompt: "Invalid choice. Try again.",
+        Goto: Goto{
+          Next: "#Output",
+        },
+      },
+      Property: Property{
+        PropertyName: "oc_bIsFinal",
+        PropertyValue: "1",
+      },
+    },
+  }
+
+  output, _ := xml.MarshalIndent(vxml, "  ", "    ")
+  output = []byte(xml.Header + string(output))
+  return output
+}
